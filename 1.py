990201530919d18ab11ef2e33a12a2efa387176d
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-27 13:46:37 +0530

    py

---
+++ 1.py
@@ -0,0 +1,5 @@
+#taking multiple inputs from user
+x,y,z=input("enter 3 numbers: ").split()
+print(x)
+print(y)
+print(z)
---
+++ hello.py
@@ -0,0 +1,15 @@
+#print("Hello. I am learning python")
+# is used to provide comments in python
+#val=input("enter any number: ")
+#print(type(val))
+#whatever we take as input from the user, it is converted to string
+
+#taking input as int or float value
+val2=int(input("enter any desired number: "))
+print(type(val2))
+
+val3=float(input("enter any float number: "))
+print(val3)
+print(type(val3))
+
+print(val2+val3)
