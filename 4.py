4230e7c094b11e2fce7d6b670308da1deb9ee061
Abhik Sahu <abhiksahu2003@gmail.com>
2023-12-31 12:54:45 +0530

    basic operators

---
+++ 4.py
@@ -0,0 +1,32 @@
+#operator in python
+print(-10/2)
+
+a=32
+b=70
+mod=b%a
+#** is used for power
+print(mod)
+
+c=2
+d=2
+print(c**d)
+
+print(a>b)#comparision
+x=True
+y=False
+print(x and y)#these are the logical operators
+print(x or y)
+print(not x)
+
+print(a & b)
+print(a | b)
+print(~a)#bitwise not operator
+print(a>>1)
+print(a<<1)
+print(a^b)
+
+list=[23,45,67,10,20]
+u=45
+v=12
+print(u not in list)
+print(v not in list)
---
+++ 5.py
@@ -0,0 +1,9 @@
+#ternary operator in python
+a,b=10,30
+min=a if a<b else b
+print(min)
+
+if a==b:
+    print("a and b are equal")
+else:
+    print("Both are unequal")
