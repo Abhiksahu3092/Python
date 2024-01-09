f08868ac4890e9045c9750bcbef470e65f3293d1
Abhik Sahu <abhiksahu2003@gmail.com>
2024-01-09 21:49:28 +0530

    operator overloading and predefined functions

---
+++ 6.py
@@ -0,0 +1,14 @@
+a=5
+b=3
+print(a/b)#this prints the float value if it occurs
+print(a//b)#this prints only int value by typecasting the quotient
+
+#operator overloading
+print("value "*4)# here '*' operator is overloaded
+print("This "+"is "+"Abhik")# here '+' operator is overloaded
+
+arr=[True,False,False,False]
+print(any(arr))
+
+arr_2=[True,True,True,True]
+print(all(arr))
---
+++ 7.py
@@ -0,0 +1,5 @@
+import operator
+
+a,b=6,2
+print(operator.add(a,b))
+#print(operator.eq(a,b))
